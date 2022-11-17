d6fd1d9d8d294712ac0acd71ad1714c3fe23f30d
James Buckley <james.buckley@polestar-eam.com>
2022-11-17 22:37:09 +0000

    Initial Commit

---
+++ FlikrApp/FlikrAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlikrAppApp.swift
+//  FlikrApp
+//
+//  Created by James Buckley on 17/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct FlikrAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
